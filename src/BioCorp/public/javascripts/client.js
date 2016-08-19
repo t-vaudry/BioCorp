015e493714122fa749d3fe640901a9b774c7d730
Mohiul Islam <mohiul.islm@hotmail.com>
2016-08-19 19:02:38 -0400

    Result Table pagination ad sorting

--- src/BioCorp/public/javascripts/client.js
+++ src/BioCorp/public/javascripts/client.js
@@ -266,24 +266,22 @@ function initializePage() {
           ondone(0);
         }
     });
-
-    if($("#results").length > 0) {
-        $("#results").dataTable();
-      	$("thead tr th:nth-child(2) p").tooltip({title:"5'-3' Sequence of the generated ribozyme"});
-      	$("thead tr th:nth-child(3) p").tooltip({title:'Hybridization temperature of the ribozymes left arm'});
-        $("thead tr th:nth-child(4) p").tooltip({title:'Hybridization temperature of the ribozymes right arm'});
-      	$("thead tr th:nth-child(5) p").tooltip({title:'Measure of target accessibility based on the RNA folding on itself. 1 is the best value, 0 is the worst.'});
-      	$("thead tr th:nth-child(6) p").tooltip({title:'Second measure of accessibility based on the thermodynamical favourability of the RNA switching to an open configuration. A greater value is better. Values will range between 0 and 1.' +
-          'This value is on a cutsite-basis.'});
-        $("thead tr th:nth-child(7) p").tooltip({title:'Measure of how good the shape is based on annealing pairs that are not in the catalytic core. A greater value is better. Values will range between 0 and 1.'});
-      	$("thead tr th:nth-child(8) p").tooltip({title:'Number of off-target hits, weighted by how good the match is and whether it is just hybridizing or fully cleaving. '
-                                         +'XN-type genes are not counted to this value, but are reported on the list. Click on the number for details. Lowest is better.'});
-      	$("thead tr th:nth-child(9) p").tooltip({title:'Overall Rank based on the quality of each attribute independently, e.g. a candidate with highest accessibility will be rank 1 regardless of everything else.'});
-
-    }
-
-
   }
+
+  if($("#results").length > 0) {
+      $("#results").dataTable();
+      $("thead tr th:nth-child(2) p").tooltip({title:"5'-3' Sequence of the generated ribozyme"});
+      $("thead tr th:nth-child(3) p").tooltip({title:'Hybridization temperature of the ribozymes left arm'});
+      $("thead tr th:nth-child(4) p").tooltip({title:'Hybridization temperature of the ribozymes right arm'});
+      $("thead tr th:nth-child(5) p").tooltip({title:'Measure of target accessibility based on the RNA folding on itself. 1 is the best value, 0 is the worst.'});
+      $("thead tr th:nth-child(6) p").tooltip({title:'Second measure of accessibility based on the thermodynamical favourability of the RNA switching to an open configuration. A greater value is better. Values will range between 0 and 1.' +
+        'This value is on a cutsite-basis.'});
+      $("thead tr th:nth-child(7) p").tooltip({title:'Measure of how good the shape is based on annealing pairs that are not in the catalytic core. A greater value is better. Values will range between 0 and 1.'});
+      $("thead tr th:nth-child(8) p").tooltip({title:'Number of off-target hits, weighted by how good the match is and whether it is just hybridizing or fully cleaving. '
+                                        +'XN-type genes are not counted to this value, but are reported on the list. Click on the number for details. Lowest is better.'});
+      $("thead tr th:nth-child(9) p").tooltip({title:'Overall Rank based on the quality of each attribute independently, e.g. a candidate with highest accessibility will be rank 1 regardless of everything else.'});
+  }
+
 
   $('#stepThreeFinish').click(function(){
     userInfoAlert.hide();
