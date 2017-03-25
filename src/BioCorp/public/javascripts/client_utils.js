0e0bdbbaed65394b0dd53d99590517e4caf3e43e
Mohiul Islam <mohiul.islm@hotmail.com>
2017-03-25 12:59:02 -0400

    readAsBinaryString defined for IE

--- src/BioCorp/public/javascripts/client_utils.js
+++ src/BioCorp/public/javascripts/client_utils.js
@@ -80,6 +80,26 @@ FileLoader.readOligoFile = function(fileToRead) {
     };
 }
 
+//extend FileReader
+if (!FileReader.prototype.readAsBinaryString) {
+    FileReader.prototype.readAsBinaryString = function (fileData) {
+       var binary = "";
+       var pt = this;
+       var reader = new FileReader();      
+       reader.onload = function (e) {
+           var bytes = new Uint8Array(reader.result);
+           var length = bytes.byteLength;
+           for (var i = 0; i < length; i++) {
+               binary += String.fromCharCode(bytes[i]);
+           }
+        //pt.result  - readonly so assign binary
+        pt.content = binary;
+        $(pt).trigger('onload');
+    }
+    reader.readAsArrayBuffer(fileData);
+    }
+}
+
 String.prototype.endsWith = function(substr){
     return this.substring(this.indexOf(substr)) == substr;
 };
